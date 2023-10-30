b8fb3b795b9f85b42fcb3e4ece7b25857a7ac3db
Guilherme Machado <machadofguilherme@gmail.com>
2023-10-30 14:10:02 -0300

    📝 docs: Gentoo

---
+++ arquivos/config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+set -gx PATH $PATH $HOME/.local/bin
+set URL "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/bubblesline.omp.json"
+
+oh-my-posh init fish --config $URL | source
+alias top "bashtop"
+
+pfetch
